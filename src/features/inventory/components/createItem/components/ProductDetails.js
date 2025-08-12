742c16cb22a4bed9f9c8ba0a3c9779597f3cd856
cjdeniz9 <ChristianDeniz9@gmail.com>
2025-08-12 14:04:01 -0700

    Testing Product Details component on mobile

--- src/features/inventory/components/createItem/components/ProductDetails.js
+++ src/features/inventory/components/createItem/components/ProductDetails.js
@@ -42,7 +42,7 @@ export default function ProductDetails() {
         variant="unstyled"
         index={tabIndex}
         onChange={handleTabChange}
-        minH="40vh"
+        minH="100vh"
       >
         <TabList>
           <Tab fontSize="14px" _selected={{ fontWeight: "600" }}>
